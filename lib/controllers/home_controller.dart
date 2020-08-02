81ef39a5cacd5a96acf6ba64c8adcace0f3eb4ca
Pedro Sousa <ppls2106@gmail.com>
2020-08-02 17:38:36 -0300

    modificando para tipo explicito

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -3,25 +3,35 @@ import 'package:borrowed_stuff/models/stuff.dart';
 
 class HomeController {
   final _stuffDAO = StuffDAO();
-  final stuffList = [];
+  final stuffList = List<Stuff>();
 
   void create(Stuff stuff) {
     _stuffDAO.create(stuff);
     stuffList.add(stuff);
+    _ordenarLista();
   }
 
   Future readAll() async {
     final list = await _stuffDAO.readAll();
     stuffList.addAll(list);
+    _ordenarLista();
   }
 
   void update(int index, Stuff stuff) {
     _stuffDAO.update(stuff);
     stuffList[index] = stuff;
+    _ordenarLista();
   }
 
   void delete(Stuff stuff) {
     _stuffDAO.delete(stuff);
     stuffList.remove(stuff);
+    _ordenarLista();
+  }
+
+  _ordenarLista() {
+    print(stuffList);
+    stuffList.sort((a, b) => b.loanDate.compareTo(a.loanDate));
+    print(stuffList);
   }
 }
