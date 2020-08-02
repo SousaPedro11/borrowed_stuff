d6d0f456f42202949fabcb1a4878c42887eb02b0
Pedro Sousa <ppls2106@gmail.com>
2020-08-02 14:54:29 -0300

    criando widget inicial para sanar erros da homepage

---
+++ lib/pages/stuff_detail_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StuffDetailPage extends StatefulWidget {
+  @override
+  _StuffDetailPageState createState() => _StuffDetailPageState();
+}
+
+class _StuffDetailPageState extends State<StuffDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
